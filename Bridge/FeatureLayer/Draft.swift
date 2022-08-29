6a901f4360a54d7fa6447ff1783c245600954c53
wansook0316 <wansook0316@gmail.com>
2022-08-29 09:47:27 +0900

    Add: Draft class 추가

--- Bridge/FeatureLayer/Draft.swift
+++ Bridge/FeatureLayer/Draft.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+internal class Draft {
+
+    internal let title: String
+    internal let author: String
+    internal let contents: [String]
+
+    internal init(title: String,
+                  author: String,
+                  contents: [String]) {
+        self.title = title
+        self.author = author
+        self.contents = contents
+    }
+
+    // 출력의 구현은 Display protocol을 구현한 쪽에서 담당함
+    internal func print(Display: display) {
+        display.title(self)
+        display.author(self)
+        display.contents(self)
+    }
+
+}
