8f259767ce2f8c6a7659acc81e0a685b095d2f08
wansook0316 <wansook0316@gmail.com>
2022-08-29 09:57:10 +0900

    Add: Display 구현체 추가

--- Bridge/ImplementationLayer/CaptionDisplay.swift
+++ Bridge/ImplementationLayer/CaptionDisplay.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+internal struct CaptionDisplay: Display {
+
+    internal func title(draft: Draft) {
+        print("Title: \(draft.title)")
+    }
+
+    internal func author(draft: Draft) {
+        print("Author: \(draft.author)")
+    }
+
+    internal func contents(draft: Draft) {
+        print("Contents: ")
+        draft.contents.forEach { content in
+            print("     \(content)")
+        }
+    }
+
+}
--- Bridge/ImplementationLayer/SimpleDisplay.swift
+++ Bridge/ImplementationLayer/SimpleDisplay.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+internal struct SimpleDisplay: Display {
+
+    internal func title(draft: Draft) {
+        print(draft.title)
+    }
+
+    internal func author(draft: Draft) {
+        print(draft.author)
+    }
+
+    internal func contents(draft: Draft) {
+        draft.contents.forEach { content in
+            print(content)
+        }
+    }
+
+}
